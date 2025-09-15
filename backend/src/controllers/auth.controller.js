0dd2d4a54eddefb8e47a2d13ddc03505101b3dd4
yaaqout <yaaqout047dev@gmail.com>
2025-09-15 22:29:39 +0300

    signup section refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -32,8 +32,9 @@ export const signup = async (req, res) => {
     });
 
     if(newUser){
-      generateToken(newUser._id, res);
-      await newUser.save()
+      const savedUser = await newUser.save();
+      generateToken(savedUser._id, res);
+
       res.status(201).json({_id: newUser._id, fullName: newUser.fullName, email: newUser.email,profilePic: newUser.profilePic});
     }else{
       return res.status(400).json({message: "Invalid user data"});
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,6 +2,10 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try{
+    const {MONGO_URI} = process.env;
+    if(!MONGO_URI){
+      throw new Error('MONGO_URI is not defined');
+    }
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log('DB connected successfully: ', conn.connection.host);
   }catch(err){
